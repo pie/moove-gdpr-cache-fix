024907ff56f87eeddb3b50bf6ad0ce2acc2eaffa
Joey <joey@pie.co.de>
2022-04-13 12:56:04 +0100

    init

---
+++ js/cache-gdpr.js
@@ -0,0 +1,87 @@
+var localCache = {
+	/**
+	* timeout for cache in millis
+	* @type {number}
+	*/
+	timeout: 1000*60*60,
+	/**
+	* @type {{_: number, data: {}}}
+	**/
+	data: {},
+	remove: function (url, dataStore) {
+		dataStore.removeItem(url);
+	},
+	exist: function (url, dataStore) {
+		console.log('Existing in cache for url ' + url);
+		if ( dataStore.getItem( url ) ) console.log( ( (new Date().getTime() - dataStore.getItem( url )._) < localCache.timeout) );
+		return dataStore.getItem( url ); // && ((new Date().getTime() - dataStore.getItem( url )._) < localCache.timeout);
+	},
+	get: function (url, dataStore) {
+		console.log('Getting in cache for url ' + url);
+		return JSON.parse( dataStore.getItem( url ) ).data;
+	},
+	set: function (url, cachedData, dataStore, callback) {
+		dataStore.removeItem( url );
+		dataStore.setItem( url, JSON.stringify( {
+			_: new Date().getTime(),
+			data: cachedData
+		}));
+		if (jQuery.isFunction(callback)) callback(cachedData);
+	}
+};
+
+jQuery.ajaxPrefilter(function (options, originalOptions, jqXHR, a) {
+
+	if ( ! window.localStorage ) return true;
+	if (
+		options.data.indexOf("action=moove_gdpr_get_scripts") === -1 &&
+	 	options.data.indexOf("action=moove_gdpr_localize_scripts") === -1 &&
+		options.data.indexOf("action=moove_gdpr_remove_php_cookies") === -1
+		)
+ 	return true;
+
+	console.log( 35, options, originalOptions, jqXHR, a );
+
+	var dataStore = window.localStorage;
+
+	if ( options.cache && dataStore ) {
+		options.cache = false;
+	}
+
+	if ( dataStore ){
+
+
+		var complete = originalOptions.complete || jQuery.noop,
+		url = originalOptions.url;
+
+		//remove jQuery cache as we have our own localCache
+		var action;
+
+		if ( options.data.indexOf("action=moove_gdpr_remove_php_cookies") > -1 ){
+			console.log( 'reset' );
+			dataStore.removeItem('moove_gdpr_get_scripts');
+			dataStore.removeItem('moove_gdpr_localize_scripts');
+			return true;
+		}
+
+		action = options.data.indexOf("action=moove_gdpr_get_scripts") > -1 ? 'moove_gdpr_get_scripts' : 'moove_gdpr_localize_scripts';
+
+
+
+		options.beforeSend = function () {
+			console.log( 'checking checking', action );
+			if (localCache.exist(action,dataStore )) {
+				console.log( 'found' );
+				complete(localCache.get(action,dataStore));
+				return false;
+			}
+			console.log( 'not found' );
+			return true;
+		};
+		options.complete = function (data, textStatus) {
+			console.log( 'setting', action, data, textStatus );
+			localCache.set(action, data, dataStore, complete);
+		};
+	}
+
+});
---
+++ moove-gdpr-cache-fix.php
@@ -0,0 +1,28 @@
+<?php
+/*
+Plugin Name: Moove GDPR Cache Fix
+Description: Adds a fix to to cache the Moove GDPR post requests client side
+Version:     0.1
+Author:      The team at PIE
+Author URI:  http://pie.co.de
+License:     GPL3
+License URI: https://www.gnu.org/licenses/gpl-3.0.html
+*/
+
+/* PIE\MooveGDPRCacheFix  is free software: you can redistribute it and/or modify it under the terms of the GNU General License as published by the Free Software Foundation, either version 2 of the License, or any later version.
+
+PIE\MooveGDPRCacheFix  is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General License for more details.
+
+You should have received a copy of the GNU General License along with PIE\MooveGDPRCacheFix . If not, see https://www.gnu.org/licenses/gpl-3.0.en.html */
+
+namespace PIE\MooveGDPRCacheFix;
+
+/**
+ * Load in JS for admin screen
+ */
+function enqueue_scripts() {
+  if ( ! is_admin() ){
+    wp_enqueue_script( 'moove-gdpr-cache-fix', plugins_url( '/js/cache-gdpr.js', __FILE__ ), array( 'jquery' ), $this->version, true );
+  }
+}
+add_action( 'wp_enqueue_scripts', 'PIE\MooveGDPRCacheFix\enqueue_scripts' );
